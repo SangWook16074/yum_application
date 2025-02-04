b5e53089e51155c13bc7a3546b9045cb5a69edd4
formart12 <formart12@naver.com>
2025-02-04 18:46:12 +0900

    Challenge Completed View 생성

--- lib/src/app/page/app_page.dart
+++ lib/src/app/page/app_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:yum_application/src/app/view/app.dart';
 import 'package:yum_application/src/app/viewModel/app_view_model.dart';
+import 'package:yum_application/src/challenge/view/challenge_completed_view.dart';
 
 class AppPage extends StatelessWidget {
   const AppPage({super.key});
@@ -10,7 +11,7 @@ class AppPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (context) => AppViewModel(),
-      child: const App(),
+      child: const ChallengeCompletedView(),
     );
   }
 }
