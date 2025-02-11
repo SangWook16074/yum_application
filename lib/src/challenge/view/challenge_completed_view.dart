108b850925c60b3b0e5b21ee9319226ef6cd7d57
formart12 <formart12@naver.com>
2025-02-11 18:30:02 +0900

    Challenge Reward Card 생성

--- lib/src/challenge/view/challenge_completed_view.dart
+++ lib/src/challenge/view/challenge_completed_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:yum_application/src/challenge/view/challenge_list_view.dart';
 import 'package:yum_application/src/challenge/widget/challenge_calendar.dart';
 import 'package:yum_application/src/challenge/widget/challenge_ranking.dart';
+import 'package:yum_application/src/challenge/widget/challenge_reward_card.dart';
 
 class ChallengeCompletedView extends StatefulWidget {
   const ChallengeCompletedView({super.key});
@@ -21,7 +22,6 @@ class _ChallengeCompletedViewState extends State<ChallengeCompletedView> {
           child: CustomScrollView(
             slivers: [
               _header(),
-              _mychallenge(),
               _challengeCompleted(),
             ],
           ),
@@ -127,89 +127,11 @@ class _ChallengeCompletedViewState extends State<ChallengeCompletedView> {
   }
 }
 
-Widget _mychallenge() {
-  PageController pageController =
-      PageController(initialPage: 1000, viewportFraction: 0.7);
-  List<String> images = [
-    "assets/images/tomato_pasta.png",
-    "assets/images/spicy_sausage_stew.png",
-    "assets/images/salmon_salad.png",
-    "assets/images/egg_fried_rice.png",
-    "assets/images/handmade_hamburger.png",
-  ];
-
-  return SliverToBoxAdapter(
-    child: StatefulBuilder(builder: (context, setState) {
-      return Column(
-        children: [
-          SizedBox(
-            width: double.infinity,
-            child: Padding(
-              padding: const EdgeInsets.only(top: 40, bottom: 10),
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Text(
-                  "나의 챌린지 현황",
-                  style: Theme.of(context).textTheme.headlineMedium,
-                ),
-              ),
-            ),
-          ),
-          SizedBox(
-            width: double.infinity,
-            height: 200, // 컨테이너 크기
-
-            child: PageView.builder(
-              controller: pageController,
-              itemBuilder: (context, index) {
-                int actualIndex = index % images.length; // 무한 루프처럼 보이게 만들기
-
-                return AnimatedBuilder(
-                  animation: pageController,
-                  builder: (context, child) {
-                    double scale = 1.0;
-                    if (pageController.hasClients) {
-                      double page = pageController.page ?? 1000;
-                      scale = (1 - (page - index).abs() * 0.3).clamp(0.8, 1.0);
-                    } else {
-                      scale = index == 1000 ? 1.0 : 0.8; // 초기 진입 시 중앙만 크게
-                    }
-
-                    return Center(
-                      child: Container(
-                        decoration: BoxDecoration(
-                            color: Theme.of(context).colorScheme.secondary,
-                            borderRadius: const BorderRadius.only(
-                                topLeft: Radius.circular(20),
-                                topRight: Radius.circular(20))),
-                        child: AnimatedContainer(
-                          duration: const Duration(milliseconds: 0),
-                          width: 300 * scale, // 중앙일 때 커짐
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(20),
-                            image: DecorationImage(
-                              image: AssetImage(images[actualIndex]),
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                      ),
-                    );
-                  },
-                );
-              },
-            ),
-          ),
-        ],
-      );
-    }),
-  );
-}
-
 Widget _challengeCompleted() {
   return const SliverToBoxAdapter(
     child: Column(
       children: [
+        ChallengeRewardCard(),
         ChallengeRanking(),
         ChallengeCalendar(),
       ],
